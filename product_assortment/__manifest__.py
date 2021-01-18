d509a8d7db92818009bde2154562b83d0d12d12a
Baptiste Chaudat <bchaudat@yziact.fr>
2021-01-18 14:24:20 +0100

    [ADD]Product attribute OCA

---
+++ product_assortment/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018-2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Assortment",
+    "summary": """
+        Adds the ability to manage products assortment""",
+    "version": "13.0.2.0.1",
+    "license": "AGPL-3",
+    "development_status": "Production/Stable",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["base", "product"],
+    "data": ["views/product_assortment.xml", "views/res_partner_view.xml"],
+    "installable": True,
+}
---
+++ product_manufacturer/readme/CONTRIBUTORS.rst
@@ -0,0 +1,6 @@
+* Odoo S.A.
+* Acysos SL <info@acysos.com>
+* Ray Carnes <ray.carnes@bistasolutions.com>
+* Miquel Raïch <miquel.raich@forgeflow.com>
+* Lois Rilo <lois.rilo@forgeflow.com>
+* Guewen Baconnier <guewen.baconnier@camptocamp.com>
